f12fff2718ed6fdb46832c51b77338342dc7a8d7
Steven Vergenz <steven.vergenz@microsoft.com>
2018-12-03 11:24:49 -0800

    Include webhost util class, auto-build on install (#1)
    * Include webhost util class, auto-build on install
    
    * Change prepare hooks to prepublishOnly

---
+++ node/packages/sdk/src/webHost.ts
@@ -0,0 +1,57 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import * as http from 'http';
+import * as Restify from 'restify';
+import { Adapter, Logger, MultipeerAdapter, NullLogger } from '.';
+
+/**
+ * Sets up an HTTP server, and generates an MRE context for your app to use.
+ */
+export class WebHost {
+    // tslint:disable:variable-name
+    private _adapter: Adapter;
+    private _baseDir: string;
+    private _baseUrl: string;
+    // tslint:enable:variable-name
+
+    public get adapter() { return this._adapter; }
+    public get baseDir() { return this._baseDir; }
+    public get baseUrl() { return this._baseUrl; }
+
+    public constructor(
+        options: { baseDir?: string, baseUrl?: string, port?: number, logger?: Logger }
+            = { baseDir: '.', baseUrl: null, port: 3901, logger: new NullLogger() }
+    ) {
+        this._baseDir = options.baseDir;
+        this._baseUrl = options.baseUrl;
+
+        // Create a Multi-peer adapter
+        this._adapter = new MultipeerAdapter({ port: options.port, logger: options.logger });
+
+        // Start listening for new app connections from a multi-peer client
+        this._adapter.listen()
+            .then(server => this.serveStaticFiles(server))
+            .catch(reason => options.logger.log('error', "Failed to start HTTP server: " + reason));
+    }
+
+    private serveStaticFiles(server: http.Server): void {
+        const restify = server as Restify.Server;
+        // The static files location
+        if (this._baseUrl === null) {
+            this._baseUrl =
+                process.env.BASE_URL || (
+                    process.env.WEBSITE_HOSTNAME ?
+                        `//${process.env.WEBSITE_HOSTNAME}` :
+                        restify.url.replace(/\[::\]/, '127.0.0.1')
+                );
+        }
+        // Setup static files route
+        restify.get('/*', Restify.plugins.serveStatic({
+            directory: this._baseDir,
+            default: 'index.html'
+        }));
+    }
+}
