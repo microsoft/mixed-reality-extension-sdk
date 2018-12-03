7c3c99e49111a002df8f24f7df78e86c9143707d
Eric Anderson <eanders@microsoft.com>
2018-12-03 12:38:16 -0800

    package publish settings

--- node/packages/scripts/compress.ps1
+++
@@ -1,4 +0,0 @@
-param([String]$app)
-
-Get-ChildItem . | Where-Object { $_.Name -NotMatch "node_modules|src|$app\.zip$|tsconfig\.json$|\.d\.ts$|\.js\.map$" } | Compress-Archive -Force -DestinationPath "$app.zip"
-#Get-ChildItem . | Where-Object { $_.Name -NotMatch "node_modules|src|$app\.zip$|tsconfig\.json$|\.d\.ts$|\.js\.map$" } | Write-Output
--- node/packages/scripts/deploy.ps1
+++
@@ -1,7 +0,0 @@
-param([String]$group, [String]$name, [String]$app)
-
-# Prerequisite actions. Perform these commands in the shell:
-# az login
-# az account set --subscription <subscriptionId>
-
-az webapp deployment source config-zip --resource-group $group --name $name --src "$app.zip"
