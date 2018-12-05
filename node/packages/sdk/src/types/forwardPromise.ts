42deedf70cd253131c91136e3917ab38327a9ca5
Steven Vergenz <vergenzs@gmail.com>
2018-12-04 16:06:53 -0800

    Add documentation

--- node/packages/sdk/src/types/forwardPromise.ts
+++ node/packages/sdk/src/types/forwardPromise.ts
@@ -3,10 +3,20 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * ForwardPromise is a promise that makes the result value immediately available. ForwardPromise is returned where
+ * it is safe to access the result before it has been fully initialized remotely. You can choose to wait on this
+ * promise if you need to know when the resource has been fully created on the host.
+ */
 export interface ForwardPromise<T> extends Promise<T> {
     value?: T;
 }
 
+/**
+ * Creates a new ForwardPromise from a literal value and a regular promise.
+ * @param value An early-resolved value of the promise.
+ * @param promise The promise to be converted.
+ */
 export function createForwardPromise<T>(
     value: T,
     promise: Promise<T>
