e479f7475019e6375dda9e066063cc18c15d38fb
Eric Anderson <eanders@microsoft.com>
2019-01-15 22:44:29 -0800

    update a method name

--- packages/sdk/src/utils/filterEmpty.ts
+++ packages/sdk/src/utils/filterEmpty.ts
@@ -7,7 +7,7 @@
  * @hidden
  * If `obj` is an empty object, return undefined.
  */
-export default function checkNonEmpty(obj: any) {
+export default function filterEmpty(obj: any) {
     if (typeof obj !== "object" || Object.keys(obj).length) {
         return obj;
     }
