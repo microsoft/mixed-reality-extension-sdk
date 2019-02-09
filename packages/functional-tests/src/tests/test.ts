397ca80e32011abe32de6612f904319dc7b05511
Steven Vergenz <steven.vergenz@microsoft.com>
2019-02-08 16:19:13 -0800

    Stub out test structure

--- packages/functional-tests/src/tests/test.ts
+++ packages/functional-tests/src/tests/test.ts
@@ -5,12 +5,59 @@
 
 import App from '../app';
 
+/**
+ * The super-class of all functional tests
+ */
 export default abstract class Test {
 
-    constructor(protected app: App) {
+    /**
+     * A human-readable description of what should be happening.
+     * Will be displayed as part of the app.
+     */
+    public expectedResultDescription: string;
+
+    constructor(protected app: App) { }
+
+    /**
+     * Main test entry point. This should run indefinitely until [[stop]] is called.
+     */
+    public abstract run(): Promise<boolean>;
+
+    /**
+     * If the test requires anything other than actor cleanup, do it here.
+     */
+    public cleanup(): void { }
+
+
+    private stoppedContinue: () => void = null;
+
+    /**
+     * Called by the test runner to end the test. Tests should not override this
+     * directly, but instead either check the _stopped variable, or await stopped().
+     */
+    public stop(): void {
+        this._stopped = true;
+        if (this.stoppedContinue) {
+            this.stoppedContinue();
+            this.stoppedContinue = null;
+        }
     }
 
-    public abstract run(): Promise<boolean>;
-    public cleanup() {
+    /**
+     * Synchronous test for when a test should stop
+     */
+    protected _stopped = false;
+
+    /**
+     * Asynchronous test for when a test should stop
+     */
+    protected stopped(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            if (this._stopped) {
+                resolve();
+            } else {
+                this.stoppedContinue = resolve;
+            }
+        });
     }
 }
