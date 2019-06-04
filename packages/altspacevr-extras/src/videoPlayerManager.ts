ea68a089b26f659cc132192bd273ba9717ce26ce
qulingyi <lingyiququ@gmail.com>
2019-06-04 16:08:52 -0700

    add show and hide function

--- packages/altspacevr-extras/src/videoPlayerManager.ts
+++ packages/altspacevr-extras/src/videoPlayerManager.ts
@@ -12,6 +12,7 @@ import {
     ContextRPC,
     UserRPC
 } from '@microsoft/mixed-reality-extension-sdk/built/rpc';
+import { debug } from 'util';
 
 type Video = { url: string; basisTime: number };
 type VideoList = { [actorId: string]: Video };
@@ -24,6 +25,7 @@ export class VideoPlayerManager {
     private _RPC: ContextRPC;
     private hasAnyUserJoined = false;
     private videos: VideoList = {};
+    private state: string;
 
     constructor(private context: Context) {
         if (context.users.length > 0) {
@@ -41,11 +43,38 @@ export class VideoPlayerManager {
         this._RPC.cleanup();
     }
 
+    // private userJoined(user: User) {
+    //     if (Object.keys(this.videos).length > 0) {
+    //         const userRPC = new UserRPC(user);
+    //         for (const actorID in this.videos) {
+    //             if (this.videos.hasOwnProperty(actorID)) {
+    //                 userRPC.emit('VideoPlay',
+    //                     {
+    //                         parentId: actorID,
+    //                         URL: this.videos[actorID].url,
+    //                         startTime: this.videos[actorID].basisTime + Date.now() / 1000.0
+    //                     });
+    //             }
+    //         }
+    //     }
+    //     this.hasAnyUserJoined = true;
+    // }
+
     private userJoined(user: User) {
-        if (Object.keys(this.videos).length > 0) {
-            const userRPC = new UserRPC(user);
-            for (const actorID in this.videos) {
-                if (this.videos.hasOwnProperty(actorID)) {
+        this.hasAnyUserJoined = true;
+        const userRPC = new UserRPC(user);
+        for (const actorID in this.videos) {
+            if (this.videos.hasOwnProperty(actorID)) {
+                if (this.state === "show") {
+                    userRPC.emit('VideoShow',
+                        {
+                            parentId: actorID,
+                            URL: this.videos[actorID].url,
+                            startTime: this.videos[actorID].basisTime + Date.now() / 1000.0,
+                            showScreen: true
+                        });
+                }
+                if (this.state === "play") {
                     userRPC.emit('VideoPlay',
                         {
                             parentId: actorID,
@@ -55,24 +84,32 @@ export class VideoPlayerManager {
                 }
             }
         }
-        this.hasAnyUserJoined = true;
     }
 
     public play(actorID: string, url: string, startTime: number) {
-        if (this.videos[actorID] === undefined || this.videos[actorID].url !== url) {
-            this.videos[actorID] = { url, basisTime: startTime - Date.now() / 1000.0 };
-            if (this.hasAnyUserJoined) {
-                this._RPC.emit('VideoPlay', { parentId: actorID, URL: url, startTime });
-            }
+        this.state = "play";
+        if (this.hasAnyUserJoined) {
+            this._RPC.emit('VideoPlay', { parentId: actorID, URL: url, startTime });
         }
     }
 
     public stop(actorID: string) {
-        if (this.videos[actorID] !== undefined) {
-            if (this.hasAnyUserJoined) {
-                this._RPC.emit('VideoPlay', { parentId: actorID, URL: "", startTime: 0.0 });
-            }
-            delete this.videos[actorID];
+        if (this.hasAnyUserJoined) {
+            this._RPC.emit('VideoPlay', { parentId: actorID, URL: "", startTime: 0.0 });
         }
+    }
+
+    public show(actorID: string, url: string, startTime: number, isShowing: boolean) {
+        this.state = "show";
+        if (this.videos[actorID] === undefined || this.videos[actorID].url !== url) {
+            this.videos[actorID] = { url, basisTime: startTime - Date.now() / 1000.0 };
+            if (this.hasAnyUserJoined) {
+                this._RPC.emit('VideoShow', { parentId: actorID, URL: url, startTime, showScreen: isShowing });
+            }
+        }
+    }
+
+    public hide(actorID: string, url: string, startTime: number, isShowing: boolean) {
+            this._RPC.emit('VideoShow', { parentId: actorID, URL: url, startTime, showScreen: isShowing });
     }
 }
