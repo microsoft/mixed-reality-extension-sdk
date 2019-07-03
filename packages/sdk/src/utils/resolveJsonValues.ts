7e5ca014a388ff6a233627b22a31ae590884818b
Steven Vergenz <steven.vergenz@microsoft.com>
2019-07-03 10:31:14 -0700

    Fix tslint, missed files

--- packages/sdk/src/utils/resolveJsonValues.ts
+++ packages/sdk/src/utils/resolveJsonValues.ts
@@ -3,11 +3,11 @@
  * Licensed under the MIT License.
  */
 
- /**
-  * @hidden
-  * Recursively look for values with a `toJSON()` method. If found,
-  * call it and replace the value with the return value.
-  */
+/**
+ * @hidden
+ * Recursively look for values with a `toJSON()` method. If found,
+ * call it and replace the value with the return value.
+ */
 export default function resolveJsonValues(obj: any) {
 	if (typeof obj === 'object') {
 		if (typeof obj.toJSON === 'function') {
