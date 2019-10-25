bb221c7d01e34d12f30de895c331cb4204b9d794
Steven Vergenz <steven.vergenz@microsoft.com>
2019-10-25 16:17:26 -0700

    Add platform to collision layer test

--- packages/functional-tests/src/tests/collision-layer-test.ts
+++ packages/functional-tests/src/tests/collision-layer-test.ts
@@ -24,6 +24,19 @@ export default class CollisionLayerTest extends Test {
 		const layers = Object.values(MRE.CollisionLayer);
 		const tableLayout = new TableLayout(5, 5, 0.2, 0.5);
 
+		MRE.Actor.CreatePrimitive(new MRE.AssetContainer(this.app.context), {
+			definition: {
+				shape: MRE.PrimitiveShape.Box,
+				dimensions: { x: 1, y: 0.1, z: 1 }
+			},
+			actor: {
+				name: 'platform',
+				parentId: root.id,
+				transform: { local: { position: { x: 2 } } },
+				collider: { geometry: { shape: 'auto' }, layer: MRE.CollisionLayer.Navigation }
+			}
+		});
+
 		// place column headers
 		for (let i = 0; i < layers.length; i++) {
 			tableLayout.setCellContents(0, 1 + i, MRE.Actor.Create(this.app.context, {
