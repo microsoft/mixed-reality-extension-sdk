ab52dba060251db38045440cfca9af7cdfeb3d74
Steven Vergenz <steven.vergenz@microsoft.com>
2019-11-12 10:28:16 -0800

    Tiny work

--- packages/sdk/src/animation/animation.ts
+++ packages/sdk/src/animation/animation.ts
@@ -10,12 +10,37 @@ import { Patchable } from '../types/patchable';
 export interface AnimationLike {
 	/** Generated unique ID */
 	id: Readonly<Guid>;
+	/** The current playback time, based on start time and speed */
 	time: number;
+	/** Playback speed multiplier */
 	speed: number;
+	/** When multiple animations play together, this is the relative strength of this instance */
 	weight: number;
+	/** What happens when the animation hits the last frame */
 	wrapMode: AnimationWrapMode;
 }
 
+/** A runtime animation */
 export class Animation implements AnimationLike, Patchable<AnimationLike> {
-
+	private _id: Guid;
+	private _basisTime: number;
+	private _speed = 0;
+	private _weight = 1;
+	private _wrapMode = AnimationWrapMode.Once;
+	/** @inheritdoc */
+	public get id() { return this._id; }
+	/** @inheritdoc */
+	public get time() {
+		if (this._speed !== 0) {
+			return (Date.now() - this._basisTime) / this._speed;
+		} else {
+			return this._basisTime;
+		}
+	}
+	/** @inheritdoc */
+	public get speed() { return this._speed; }
+	/** @inheritdoc */
+	public get weight() { return this._weight; }
+	/** @inheritdoc */
+	public get wrapMode() { return this._wrapMode; }
 }
