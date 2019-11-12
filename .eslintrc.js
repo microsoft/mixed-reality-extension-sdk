a0715850a469443bac89abb79bad5e041c21021c
Steven Vergenz <steven.vergenz@microsoft.com>
2019-11-12 11:57:33 -0800

    Add starter eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended"
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
