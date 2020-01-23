d77632a21b5b5abd749073de02136199bafc4da3
Steven Vergenz <steven.vergenz@microsoft.com>
2020-01-23 11:30:55 -0800

    Fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+	"root": true,
 	"env": {
 		"es6": true,
 		"node": true
@@ -15,6 +16,7 @@ module.exports = {
 	},
 	"parser": "@typescript-eslint/parser",
 	"parserOptions": {
+		"tsconfigRootDir": __dirname,
 		"ecmaVersion": 2018,
 		"sourceType": "module",
 		"project": "./packages/*/tsconfig.json"
