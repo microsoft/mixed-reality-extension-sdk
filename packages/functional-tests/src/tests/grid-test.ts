1fe70e476a36081887377f025ac455f85eedc890
Steven Vergenz <steven.vergenz@microsoft.com>
2020-02-25 09:33:17 -0800

    Add docstrings, re-add cell alignment

--- packages/functional-tests/src/tests/grid-test.ts
+++ packages/functional-tests/src/tests/grid-test.ts
@@ -7,7 +7,7 @@ import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 import { Test } from '../test';
 
 export default class GridTest extends Test {
-	public expectedResultDescription = "Lay out actors in a grid";
+	public expectedResultDescription = "Click balls to realign grid";
 	private assets: MRE.AssetContainer;
 
 	public async run(root: MRE.Actor): Promise<boolean> {
@@ -33,14 +33,34 @@ export default class GridTest extends Test {
 				collider: { geometry: { shape: MRE.ColliderType.Auto } }
 			}});
 			button.setBehavior(MRE.ButtonBehavior).onClick(() => {
-				console.log(alignment);
 				buttonGrid.gridAlignment = alignment;
 				buttonGrid.applyLayout();
 			});
 			buttonGrid.addCell({
-				row: Math.floor(i / 3), column: i % 3,
-				width: 0.3, height: 0.3,
+				row: Math.floor(i / 3),
+				column: i % 3,
+				width: 0.3,
+				height: 0.3,
 				contents: button
+			});
+
+			const label = MRE.Actor.Create(this.app.context, { actor: {
+				name: "label",
+				parentId: anchor.id,
+				text: {
+					contents: GridTest.ShortName(alignment),
+					height: 0.05,
+					anchor: GridTest.BoxToTextAlignment(alignment),
+					color: MRE.Color3.Teal()
+				}
+			}});
+			buttonGrid.addCell({
+				row: Math.floor(i / 3),
+				column: i % 3,
+				width: 0.3,
+				height: 0.3,
+				alignment,
+				contents: label
 			});
 		}
 		buttonGrid.applyLayout();
@@ -51,5 +71,23 @@ export default class GridTest extends Test {
 
 	public cleanup() {
 		this.assets.unload();
+	}
+
+	private static ShortName(align: MRE.BoxAlignment) {
+		return align.charAt(0) + /-(.)/u.exec(align)[1];
+	}
+
+	private static BoxToTextAlignment(boxAlign: MRE.BoxAlignment) {
+		switch (boxAlign) {
+			case MRE.BoxAlignment.TopLeft: return MRE.TextAnchorLocation.BottomRight;
+			case MRE.BoxAlignment.TopCenter: return MRE.TextAnchorLocation.BottomCenter;
+			case MRE.BoxAlignment.TopRight: return MRE.TextAnchorLocation.BottomLeft;
+			case MRE.BoxAlignment.MiddleLeft: return MRE.TextAnchorLocation.MiddleRight;
+			case MRE.BoxAlignment.MiddleCenter: return MRE.TextAnchorLocation.MiddleCenter;
+			case MRE.BoxAlignment.MiddleRight: return MRE.TextAnchorLocation.MiddleLeft;
+			case MRE.BoxAlignment.BottomLeft: return MRE.TextAnchorLocation.TopRight;
+			case MRE.BoxAlignment.BottomCenter: return MRE.TextAnchorLocation.TopCenter;
+			case MRE.BoxAlignment.BottomRight: return MRE.TextAnchorLocation.TopLeft;
+		}
 	}
 }
--- packages/sdk/src/util/gridLayout.ts
+++ packages/sdk/src/util/gridLayout.ts
@@ -4,25 +4,41 @@
  */
 import { Actor, Vector3 } from '..';
 
-
+/** Describes a relative position in a [[GridLayout]]. */
 export enum BoxAlignment {
+	/** Position above and to the left of the anchor. */
 	TopLeft = 'top-left',
+	/** Position directly above the anchor. */
 	TopCenter = 'top-center',
+	/** Position above and to the right of the anchor. */
 	TopRight = 'top-right',
+	/** Position directly left of the anchor. */
 	MiddleLeft = 'middle-left',
+	/** Position directly on top of the anchor. */
 	MiddleCenter = 'middle-center',
+	/** Position directly right of the anchor. */
 	MiddleRight = 'middle-right',
+	/** Position below and to the left of the anchor. */
 	BottomLeft = 'bottom-left',
+	/** Position directly below the anchor. */
 	BottomCenter = 'bottom-center',
+	/** Position below and to the right of the anchor. */
 	BottomRight = 'bottom-right',
 }
 
+/** Options for [[GridLayout.addCell]]. */
 export interface AddCellOptions {
+	/** The actor to be placed in the grid cell. Must be parented to the grid root. */
 	contents: Actor;
+	/** The row index, with 0 at the top. */
 	row: number;
+	/** The column index, with 0 on the left. */
 	column: number;
+	/** The width of this cell for layout purposes. Should include any desired padding. */
 	width: number;
+	/** The height of this cell for layout purposes. Should include any desired padding. */
 	height: number;
+	/** Where the actor should be placed within the cell box. Defaults to [[GridLayout.defaultCellAlignment]]. */
 	alignment?: BoxAlignment;
 }
 
@@ -39,88 +55,112 @@ export class GridLayout {
 	 * Initialize a new grid layout.
 	 * @param anchor The grid's anchor actor, the point to which the grid is aligned.
 	 * @param gridAlignment How the grid should be aligned to its anchor, where [[BoxAlignment.TopLeft]] will place
-	 * the grid above and to the left of the anchor.
+	 * the grid above and to the left of the anchor, and the lower right corner will touch the anchor.
 	 */
 	public constructor(
 		private anchor: Actor,
 		public gridAlignment = BoxAlignment.MiddleCenter,
+		public defaultCellAlignment = BoxAlignment.MiddleCenter
 	) { }
 
-	public tableWidth() { return this.columns.reduce((sum, x) => sum + x, 0); }
-	public tableHeight() { return this.rows.reduce((sum, x) => sum + x, 0); }
-	public columnWidth(i: number) { return this.columns[i] ?? 0; }
-	public rowHeight(i: number) { return this.rows[i] ?? 0; }
+	/** The number of columns in this grid. */
+	public columnCount() { return this.columns.length; }
+	/** THe number of rows in this grid. */
+	public rowCount() { return this.rows.length; }
+	/** The width of the full grid. */
+	public gridWidth() { return this.columns.reduce((sum, x) => sum + x, 0); }
+	/** The height of the full grid. */
+	public gridHeight() { return this.rows.reduce((sum, x) => sum + x, 0); }
 
+	/**
+	 * The width of a particular column.
+	 * @param i The column index.
+	 */
+	public columnWidth(i: number) {
+		return this.contents
+			.filter(c => c.column === i)
+			.reduce((max, c) => Math.max(max, c.width), 0);
+	}
+
+	/**
+	 * The height of a particular row.
+	 * @param i The row index.
+	 */
+	public rowHeight(i: number) {
+		return this.contents
+			.filter(c => c.row === i)
+			.reduce((max, c) => Math.max(max, c.height), 0);
+	}
+
+	/**
+	 * Add an actor to the grid. The actor's position will not be updated until [[applyLayout]] is called.
+	 * @param options The cell's configuration.
+	 */
 	public addCell(options: AddCellOptions) {
-		const { contents, row, column, width, height } = options;
+		const { contents } = options;
 
 		if (contents.parent !== this.anchor) {
 			throw new Error("Grid cell contents must be parented to the grid root");
 		}
 
-		// update row/column dimensions
-		while (this.rows.length <= row) {
-			this.rows.push(0);
-		}
-		while (this.columns.length <= column) {
-			this.columns.push(0);
-		}
-		this.rows[row] = Math.max(this.rows[row], height);
-		this.columns[column] = Math.max(this.columns[column], width);
-
 		// insert cell
 		this.contents.push(options);
 	}
 
+	/** Recompute the positions of all actors in the grid. */
 	public applyLayout() {
-		const gridAlign = getOffsetFromAlignment(this.gridAlignment, this.tableWidth(), this.tableHeight())
+		const gridAlign = GridLayout.getOffsetFromAlignment(
+			this.gridAlignment, this.gridWidth(), this.gridHeight())
+			.multiplyByFloats(-1, -1, -1);
 
 		for (const cell of this.contents) {
 			const cellPosition = new Vector3(
-				this.columns.slice(0, cell.column).reduce((sum, x) => sum + x, 0) + this.columns[cell.column] / 2,
-				-this.rows.slice(0, cell.row).reduce((sum, x) => sum + x, 0) - this.rows[cell.row] / 2,
+				this.columns.slice(0, cell.column).reduce((sum, x) => sum + x, 0),
+				-this.rows.slice(0, cell.row).reduce((sum, x) => sum + x, 0),
 				0
 			);
-			cell.contents.transform.local.position =
-				gridAlign
-				.add(cellPosition);
+			const cellAlign = GridLayout.getOffsetFromAlignment(
+				cell.alignment ?? this.defaultCellAlignment,
+				this.columns[cell.column], this.rows[cell.row]
+			);
+			cell.contents.transform.local.position = gridAlign.add(cellPosition).add(cellAlign);
 		}
 	}
-}
 
-function getOffsetFromAlignment(anchor: BoxAlignment, width: number, height: number) {
-	let offset = new Vector3();
+	private static getOffsetFromAlignment(anchor: BoxAlignment, width: number, height: number) {
+		const offset = new Vector3();
 
-	// set horizontal alignment
-	switch(anchor) {
-		case BoxAlignment.TopRight:
-		case BoxAlignment.MiddleRight:
-		case BoxAlignment.BottomRight:
-			offset.x = 0;
-			break;
-		case BoxAlignment.TopCenter:
-		case BoxAlignment.MiddleCenter:
-		case BoxAlignment.BottomCenter:
-			offset.x = -0.5;
-			break;
-		default:
-			offset.x = -1;
+		// set horizontal alignment
+		switch(anchor) {
+			case BoxAlignment.TopRight:
+			case BoxAlignment.MiddleRight:
+			case BoxAlignment.BottomRight:
+				offset.x = 0;
+				break;
+			case BoxAlignment.TopCenter:
+			case BoxAlignment.MiddleCenter:
+			case BoxAlignment.BottomCenter:
+				offset.x = 0.5;
+				break;
+			default:
+				offset.x = 1;
+		}
+		// set vertical alignment
+		switch (anchor) {
+			case BoxAlignment.BottomLeft:
+			case BoxAlignment.BottomCenter:
+			case BoxAlignment.BottomRight:
+				offset.y = 0;
+				break;
+			case BoxAlignment.MiddleLeft:
+			case BoxAlignment.MiddleCenter:
+			case BoxAlignment.MiddleRight:
+				offset.y = -0.5;
+				break;
+			default:
+				offset.y = -1;
+		}
+
+		return offset.multiplyByFloats(width, height, 1);
 	}
-	// set vertical alignment
-	switch (anchor) {
-		case BoxAlignment.BottomLeft:
-		case BoxAlignment.BottomCenter:
-		case BoxAlignment.BottomRight:
-			offset.y = 0;
-			break;
-		case BoxAlignment.MiddleLeft:
-		case BoxAlignment.MiddleCenter:
-		case BoxAlignment.MiddleRight:
-			offset.y = 0.5;
-			break;
-		default:
-			offset.y = 1;
-	}
-
-	return offset.multiplyByFloats(width, height, 1);
 }
