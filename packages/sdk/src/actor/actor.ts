67650780ef7e5a22a7ea4f6d7884ef6e4403ba98
Steven Vergenz <steven.vergenz@microsoft.com>
2020-09-28 16:26:40 -0700

    CreateFromGltf optimization: Don't re-load assets if they already exist (#720)
    * CreateFromGltf optimization
    
    * Whoops

--- packages/sdk/src/actor/actor.ts
+++ packages/sdk/src/actor/actor.ts
@@ -315,7 +315,12 @@ export class Actor implements ActorLike, Patchable<ActorLike> {
 		colliderType?: 'box' | 'mesh';
 		actor?: Partial<ActorLike>;
 	}): Actor {
-		return container.context.internal.CreateFromGltf(container, options);
+		let prefab: Prefab;
+		if (prefab = container.prefabs.find(p => p.source.uri === options.uri)) {
+			return Actor.CreateFromPrefab(container.context, { prefab, actor: options.actor });
+		} else {
+			return container.context.internal.CreateFromGltf(container, options);
+		}
 	}
 
 	/**
