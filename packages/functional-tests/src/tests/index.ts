5af5bbc2af6667001a1ac48966330157e03e492d
Patrick O'Leary <patole@microsoft.com>
2020-12-23 16:29:09 -0800

    Pages and Folders for Functional Tests Menu (#727)
    Replaced auto-generated alphabetic pages with user-defined groups for Functional Tests

--- packages/functional-tests/src/tests/index.ts
+++ packages/functional-tests/src/tests/index.ts
@@ -57,70 +57,106 @@ import VideoTest from './video-test';
 import VideoSyncTest from './video-sync-test';
 import VisibilityTest from './visibility-test';
 
-export type FactoryMap = { [key: string]: TestFactory };
+/**
+ * Format for the map passed to the populatePages function.
+ * Keys are name strings, displayed in the test app 
+ * Values are either a TestFactory for a test, or another FactoryMap for test folders
+ */
+export type FactoryMap = { [key: string]: TestFactory | FactoryMap };
 
 /**
  * Registry of functional tests. Add your test here.
  * *** KEEP LIST SORTED ***
  */
+
 export const Factories = {
-	'actor-attachment': (...args) => new ActorAttachmentTest(...args),
-	'actor-spam': (...args) => new ActorSpamTest(...args),
-	'altspacevr-library': (...args) => new AltspaceVRLibraryTest(...args),
-	'altspacevr-user-filter': (...args) => new AltspaceVRUserFilterTest(...args),
-	'animation-blend': (...args) => new AnimationBlendTest(...args),
-	'animation-dynamic': (...args) => new AnimationDynamicTest(...args),
-	'animation-relative': (...args) => new AnimationRelativeTest(...args),
-	'animation-scale': (...args) => new AnimationScaleTest(...args),
-	'animation-sync': (...args) => new AnimationSyncTest(...args),
-	'animation-to': (...args) => new AnimationToTest(...args),
-	'asset-early-assignment': (...args) => new AssetEarlyAssignmentTest(...args),
-	'asset-mutability': (...args) => new AssetMutabilityTest(...args),
-	'asset-preload': (...args) => new AssetPreloadTest(...args),
-	'asset-unload': (...args) => new AssetUnloadTest(...args),
-	'button-targeting': (...args) => new ButtonTargetingTest(...args),
-	'clock-sync': (...args) => new ClockSyncTest(...args),
-	'collision-layer': (...args) => new CollisionLayerTest(...args),
-	'failure': (...args) => new FailureTest(...args),
-	'font': (...args) => new FontTest(...args),
-	'gltf-actor-sync': (...args) => new GltfActorSyncTest(...args),
-	'gltf-concurrency': (...args) => new GltfConcurrencyTest(...args),
-	'gltf-gen': (...args) => new GltfGenTest(...args),
-	'grab': (...args) => new GrabTest(...args),
-	'grid': (...args) => new GridTest(...args),
-	'input': (...args) => new InputTest(...args),
-	'library-fail': (...args) => new LibraryFailTest(...args),
-	'light': (...args) => new LightTest(...args),
-	'livestream': (...args) => new LivestreamTest(...args),
-	'look-at': (...args) => new LookAtTest(...args),
-	'physics-bounce': (...args) => new PhysicsBounceTest(...args),
-	'physics-friction': (...args) => new PhysicsFrictionTest(...args),
-	'physics-free-fall': (...args) => new PhysichFreeFallTest(...args),
-	'physics-shared-and-exclusive': (...args) => new PhysicsExclusiveTest(false, ...args),
-	'physics-shared-and-exclusive-with-fixed-mass': (...args) => new PhysicsExclusiveTest(true, ...args),
-	'physics-collision-b.0': (...args) => new PhysicsCollisionTest( 0, ...args),
-	'physics-collision-b.8': (...args) => new PhysicsCollisionTest( 0.8,...args),
-	'physics-head-collision-b.0': (...args) => new PhysicsHeadCollisionTest(0,...args),
-	'physics-head-collision-b.2': (...args) => new PhysicsHeadCollisionTest(0.2,...args),
-	'physics-pile-b.0v1': (...args) => new PhysicsPileTest(0, -1, 50, 0.0,...args),
-	'physics-pile-b.0v2': (...args) => new PhysicsPileTest(0, 100000, 70, 0.0,...args),
-	'physics-pile-b.0v3': (...args) => new PhysicsPileTest(0, -1, 50, 0.2,...args),
-	'physics-sim': (...args) => new PhysicsSimTest(...args),
-	'physics-stack': (...args) => new PhysicsStackTest(4, 0.5, false, false, ...args),
-	'physics-stack-mix': (...args) => new PhysicsStackTest(4, 0.5, true, false, ...args),
-	'physics-set-authority': (...args) => new PhysicsStackTest(4, 0.5, true, true, ...args),
-	'physics-dynamic-vs-kinematic': (...args) => new PhysicsDynamicVsKinematicTest(...args),
-	'primitives': (...args) => new PrimitivesTest(...args),
-	'prompt': (...args) => new PromptTest(...args),
-	'reparent': (...args) => new ReparentTest(...args),
-	'sound': (...args) => new SoundTest(...args),
-	'sound-sync': (...args) => new SoundSyncTest(...args),
-	'stats': (...args) => new StatsTest(...args),
-	'text': (...args) => new TextTest(...args),
-	'transform': (...args) => new TransformTest(...args),
-	'user-mask': (...args) => new UserMaskTest(...args),
-	'user': (...args) => new UserTest(...args),
-	'video': (...args) => new VideoTest(...args),
-	'video-sync': (...args) => new VideoSyncTest(...args),
-	'visibility': (...args) => new VisibilityTest(...args),
+	'Actor Tests': {
+		'actor-attachment': (...args) => new ActorAttachmentTest(...args),
+		'actor-spam': (...args) => new ActorSpamTest(...args),
+		'grid': (...args) => new GridTest(...args),
+		'look-at': (...args) => new LookAtTest(...args),
+		'primitives': (...args) => new PrimitivesTest(...args),
+		'reparent': (...args) => new ReparentTest(...args),
+		'transform': (...args) => new TransformTest(...args),
+		'user-mask': (...args) => new UserMaskTest(...args),
+		'visibility': (...args) => new VisibilityTest(...args),
+	},
+	'AVR Tests': {
+		'altspacevr-library': (...args) => new AltspaceVRLibraryTest(...args),
+		'altspacevr-user-filter': (...args) => new AltspaceVRUserFilterTest(...args),
+	},
+	'Animation Tests': {
+		'animation-blend': (...args) => new AnimationBlendTest(...args),
+		'animation-dynamic': (...args) => new AnimationDynamicTest(...args),
+		'animation-relative': (...args) => new AnimationRelativeTest(...args),
+		'animation-scale': (...args) => new AnimationScaleTest(...args),
+		'animation-sync': (...args) => new AnimationSyncTest(...args),
+		'animation-to': (...args) => new AnimationToTest(...args),
+		'clock-sync': (...args) => new ClockSyncTest(...args),
+	},
+	'Asset Tests': {
+		'asset-early-assignment': (...args) => new AssetEarlyAssignmentTest(...args),
+		'asset-mutability': (...args) => new AssetMutabilityTest(...args),
+		'asset-preload': (...args) => new AssetPreloadTest(...args),
+		'asset-unload': (...args) => new AssetUnloadTest(...args),
+	},
+	'GLTF Tests': {
+		'gltf-actor-sync': (...args) => new GltfActorSyncTest(...args),
+		'gltf-concurrency': (...args) => new GltfConcurrencyTest(...args),
+		'gltf-gen': (...args) => new GltfGenTest(...args),	
+	},
+	'Graphics/Rendering Tests': {
+		'light': (...args) => new LightTest(...args),
+		'text': (...args) => new TextTest(...args),
+		'font': (...args) => new FontTest(...args),
+	},
+	'Input Tests': {
+		'button-targeting': (...args) => new ButtonTargetingTest(...args),	
+		'grab': (...args) => new GrabTest(...args),
+		'input': (...args) => new InputTest(...args),
+		'prompt': (...args) => new PromptTest(...args),
+	},
+	'Media Tests': {
+		'livestream': (...args) => new LivestreamTest(...args),
+		'video': (...args) => new VideoTest(...args),
+		'video-sync': (...args) => new VideoSyncTest(...args),
+	},
+	'Physics Tests': {
+		'Collision Tests': {
+			'collision-layer': (...args) => new CollisionLayerTest(...args),
+			'physics-collision-b.0': (...args) => new PhysicsCollisionTest( 0, ...args),
+			'physics-collision-b.8': (...args) => new PhysicsCollisionTest( 0.8,...args),
+			'physics-head-collision-b.0': (...args) => new PhysicsHeadCollisionTest(0,...args),
+			'physics-head-collision-b.2': (...args) => new PhysicsHeadCollisionTest(0.2,...args),
+		},
+		'Piling/Stacking Tests': {
+			'physics-pile-b.0v1': (...args) => new PhysicsPileTest(0, -1, 50, 0.0,...args),
+			'physics-pile-b.0v2': (...args) => new PhysicsPileTest(0, 100000, 70, 0.0,...args),
+			'physics-pile-b.0v3': (...args) => new PhysicsPileTest(0, -1, 50, 0.2,...args),
+			'physics-stack': (...args) => new PhysicsStackTest(4, 0.5, false, false, ...args),
+			'physics-stack-mix': (...args) => new PhysicsStackTest(4, 0.5, true, false, ...args),
+		},
+		'Other Physics Tests': {
+			'physics-bounce': (...args) => new PhysicsBounceTest(...args),
+			'physics-friction': (...args) => new PhysicsFrictionTest(...args),
+			'physics-free-fall': (...args) => new PhysichFreeFallTest(...args),
+			'physics-sim': (...args) => new PhysicsSimTest(...args),
+			'physics-set-authority': (...args) => new PhysicsStackTest(4, 0.5, true, true, ...args),
+			'physics-dynamic-vs-kinematic': (...args) => new PhysicsDynamicVsKinematicTest(...args),
+		},
+		'Shared/Exclusive Tests': {
+			'physics-shared-and-exclusive': (...args) => new PhysicsExclusiveTest(false, ...args),
+			'physics-shared-and-exclusive-with-fixed-mass': (...args) => new PhysicsExclusiveTest(true, ...args),
+		}
+	},
+	'Sound Tests': {
+		'sound': (...args) => new SoundTest(...args),
+		'sound-sync': (...args) => new SoundSyncTest(...args),
+	},
+	'Utility Tests': {
+		'failure': (...args) => new FailureTest(...args),
+		'library-fail': (...args) => new LibraryFailTest(...args),
+		'stats': (...args) => new StatsTest(...args),
+		'user': (...args) => new UserTest(...args),
+	},
 } as FactoryMap;
