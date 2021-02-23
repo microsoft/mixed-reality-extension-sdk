6357b488b7dc208e2005ae0bc8b8534bafe5d8ee
Patrick O'Leary (HAVOK) <PATOLE@microsoft.com>
2021-02-23 13:58:13 -0800

    Sound type

--- packages/sdk/src/asset/sound.ts
+++ packages/sdk/src/asset/sound.ts
@@ -12,11 +12,13 @@ import { Asset } from './asset';
 export interface SoundLike {
 	uri: string;
 	duration: number;
+	format?: string;
 }
 
 export class Sound extends Asset implements SoundLike, Patchable<AssetLike> {
 	private _uri: string;
 	private _duration = 0;
+	private _format: string;
 	private _internal = new AssetInternal(this);
 
 	/** @hidden */
@@ -27,6 +29,9 @@ export class Sound extends Asset implements SoundLike, Patchable<AssetLike> {
 
 	/** The length of the loaded sound in seconds at default pitch */
 	public get duration() { return this._duration; }
+
+	/** Audio Format */
+	public get format() { return this._format; }
 
 	/** @inheritdoc */
 	public get sound(): Sound { return this; }
@@ -58,6 +63,9 @@ export class Sound extends Asset implements SoundLike, Patchable<AssetLike> {
 		if (from.sound && from.sound.duration !== undefined) {
 			this._duration = from.sound.duration;
 		}
+		if (from.sound && from.sound.format !== undefined) {
+			this._format = from.sound.format;
+		}
 
 		this.internal.observing = wasObserving;
 		return this;
@@ -70,6 +78,7 @@ export class Sound extends Asset implements SoundLike, Patchable<AssetLike> {
 			sound: {
 				uri: this.uri,
 				duration: this.duration,
+				format: this.format
 			}
 		};
 	}
